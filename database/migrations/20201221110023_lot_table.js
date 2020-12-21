91f87324d7db1a731c7a2b0b48732edd8b8ff387
Thomas Li <thomasligreat11@gmail.com>
2020-12-21 11:17:57 -0800

    feat: added spots table migration file

---
+++ database/migrations/20201221110023_lot_table.js
@@ -0,0 +1,20 @@
+const spot_status_enum = [
+    'UNOCCUPIED',
+    'RESERVED',
+    'OCCUPIED',
+    'OFF_LINE',
+];
+exports.up = function(knex) {
+    return knex.schema.createTable('spots', (tbl)=>{
+        tbl.increments('id').unique().notNullable();
+        tbl.text('spot_name');
+        tbl.text('secret');
+        tbl.boolean('alive_status');
+        tbl.enum('spot_status', spot_status_enum, {useNative: true, enumName:'spot_status_enum'}).notNullable().index();
+
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('spots');
+};
