a7fd9e5c0b72d0ee21381769739608d6cd55b77b
YuyusanK <kurinoyuichi@gmail.com>
2020-12-23 14:19:00 -0800

    feat: created /occupied route

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const spotsModel = require('../../database/models/spotsModel');
 
 /* /api/route this is where all the ESP8266 will make request at */
 router.get('/', function(req, res, next) {
     res.send('success');
+});
+
+router.post('/occupied', function(req, res, next){
+    const postResponse = spotsModel.updateSpotById(req.body);
+    res.send(postResponse);
 });
 
 module.exports = router;
--- database/migrations/20201221110023_lot_table.js
+++ database/migrations/20201221110023_lot_table.js
@@ -8,7 +8,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('spots', (tbl)=>{
         tbl.increments('id').unique().notNullable();
         tbl.text('spot_name');
-        tbl.text('secret');
+        tbl.text('secret');// ESP8266 hashed ID
         tbl.boolean('alive_status');
         tbl.enum('spot_status', spot_status_enum, {useNative: true, enumName:'spot_status_enum'}).notNullable().index();
 
