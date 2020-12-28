b87e68346cc704a314eb12dd26c7823cb3049759
YuyusanK <kurinoyuichi@gmail.com>
2020-12-28 14:51:30 -0800

    fix: added /occupied_status route

--- app/routes/api.js
+++ app/routes/api.js
@@ -41,18 +41,4 @@ router.post('/occupied_status', async function(req, res, next) {
     }
 });
 
-router.post('/authenticate', async function(req, res, next){
-    const secret = req.body.secret;
-    const rows = await spotsModel.getSpotBySecret(secret);
-    if (rows.length>0){
-        const spotAUTHData = {secret};
-        const token = await tokenUtil.generateToken(spotAUTHData);
-        res.clearCookie('spotInfo');
-        res.cookie("spotInfo", token);
-        res.status(202).json({status:'success', data:token});
-    } else {
-        res.status(401).json({status:'unauthorized', data:'secret not found'});
-    }
-});
-
 module.exports = router;
