2f9ca5e6130842e18ec20686f671c94bba001abf
Thomas Li <thomasligreat11@gmail.com>
2020-12-27 21:34:08 -0800

    feat: added get/api/spot route

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const spotsModel = require('../../database/models/spotsModel');
 
 /* /api/route this is where all the ESP8266 will make request at */
-router.get('/', function(req, res, next) {
-    res.send('success');
+router.get('/spot', async function(req, res, next) {
+    const {secret} = req.spotInfo;
+    const rows = await spotsModel.getSpotBySecret(secret);
+    if (rows.size === 0){
+        res.status(404)
+            .json({status:'failed', message: 'Failed to look up spot info'});
+    } else {
+        const result = rows[0];
+        res.status(200)
+            .json({status:'success', result});
+    }
 });
 
 module.exports = router;
