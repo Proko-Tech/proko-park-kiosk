b997147dfdac08d7d0eee6444c1b83bdb31f6fc9
YuyusanK <kurinoyuichi@gmail.com>
2020-12-28 02:24:08 -0800

    merge from master

--- app/routes/api.js
+++ app/routes/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const spotsModel = require('../../database/models/spotsModel');
 const tokenUtil = require('../auth/tokenUtil');
-
+const http = require('http');
 /* /api/route this is where all the ESP8266 will make request at */
 router.get('/', function(req, res, next) {
     res.send('success');
