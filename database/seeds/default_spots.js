5a051657ec27dc7bb87eab66bf8d56d07252621b
Thomas Li <thomasligreat11@gmail.com>
2021-01-07 12:40:55 -0800

    added spots

--- database/seeds/default_spots.js
+++ database/seeds/default_spots.js
@@ -8,6 +8,15 @@ exports.seed = function(knex) {
                 {id: 1, spot_name: 'spot1', secret:'dfjaksfsl', alive_status:true, spot_status:'UNOCCUPIED'},
                 {id: 2, spot_name: 'spot2', secret:'dasfasfas', alive_status:true, spot_status:'UNOCCUPIED'},
                 {id: 3, spot_name: 'spot3', secret:'afdasdfad', alive_status:true, spot_status:'UNOCCUPIED'},
+                {id: 4, spot_name: 'spot4', secret:'fdasffdad', alive_status:true, spot_status:'UNOCCUPIED'},
+                {id: 5, spot_name: 'spot5', secret:'fdsassdfd', alive_status:true, spot_status:'UNOCCUPIED'},
+                {id: 6, spot_name: 'spot6', secret:'trewtrytc', alive_status:true, spot_status:'UNOCCUPIED'},
+                {id: 7, spot_name: 'spot7', secret:'cxvbxvbbv', alive_status:true, spot_status:'UNOCCUPIED'},
+                {id: 8, spot_name: 'spot8', secret:'lkhkjgjkb', alive_status:true, spot_status:'UNOCCUPIED'},
+                {id: 9, spot_name: 'spot9', secret:'zxccvcxzn', alive_status:true, spot_status:'UNOCCUPIED'},
+                {id: 10, spot_name: 'spot10', secret:'qweerewrd', alive_status:true, spot_status:'UNOCCUPIED'},
+                {id: 11, spot_name: 'spot11', secret:'hfsdfgfdd', alive_status:true, spot_status:'UNOCCUPIED'},
+                {id: 12, spot_name: 'spot12', secret:'bvcxhncxx', alive_status:true, spot_status:'UNOCCUPIED'},
             ]);
         });
 };
