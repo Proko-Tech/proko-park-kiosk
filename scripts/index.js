8a69c9d9abbe6fbbdeea0967a280e7776ca27ee5
Thomas Li <thomasligreat11@gmail.com>
2021-09-10 11:32:06 -0700

    connect to databse

--- scripts/index.js
+++ scripts/index.js
@@ -10,16 +10,9 @@ const provider = require('../provider');
  * @returns {Promise<void>}
  */
 function startScript() {
-    schedule.scheduleJob('*/20 * * * * *', async function() {
+    schedule.scheduleJob('*/10 * * * * *', async function() {
         try {
             await spotsModel.updateLiveStatus();
-            const lot_spots = await spotsModel.getAll();
-            const response = await provider.postParkingLotRequest(lot_spots);
-            const spots = response.data.parking_lot_info.spots;
-            const result = await spotsModel.updateSpots(spots);
-            if (result.status !== 'success'){
-                console.log(result.message);
-            }
         } catch (err){
             console.log(err);
         }
