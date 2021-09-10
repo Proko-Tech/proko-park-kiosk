df31fad3b0f802cd19cb4bd3951445adbfb7c06b
Thomas Li <thomasligreat11@gmail.com>
2021-09-10 11:37:20 -0700

    removed unnecessary script

--- scripts/index.js
+++ scripts/index.js
@@ -13,6 +13,13 @@ function startScript() {
     schedule.scheduleJob('*/10 * * * * *', async function() {
         try {
             await spotsModel.updateLiveStatus();
+            // const lot_spots = await spotsModel.getAll();
+            // const response = await provider.postParkingLotRequest(lot_spots);
+            // const spots = response.data.parking_lot_info.spots;
+            // const result = await spotsModel.updateSpots(spots);
+            // if (result.status !== 'success'){
+            //     console.log(result.message);
+            // }
         } catch (err){
             console.log(err);
         }
